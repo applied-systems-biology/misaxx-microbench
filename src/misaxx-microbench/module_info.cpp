f6818bb4758003342c4e41aea866be21a49c10c3
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-12-11 20:50:03 +0100

    + Licensing

--- src/misaxx-microbench/module_info.cpp
+++ src/misaxx-microbench/module_info.cpp
@@ -1,3 +1,15 @@
+/**
+ * Copyright by Ruman Gerst
+ * Research Group Applied Systems Biology - Head: Prof. Dr. Marc Thilo Figge
+ * https://www.leibniz-hki.de/en/applied-systems-biology.html
+ * HKI-Center for Systems Biology of Infection
+ * Leibniz Institute for Natural Product Research and Infection Biology - Hans Knöll Insitute (HKI)
+ * Adolf-Reichwein-Straße 23, 07745 Jena, Germany
+ *
+ * This code is licensed under BSD 2-Clause
+ * See the LICENSE file provided with this code for the full license.
+ */
+
 #include <misaxx/core/module_info.h>
 #include <misaxx-microbench/module_info.h>
 #include <misaxx/imaging/module_info.h>
@@ -8,7 +20,7 @@ misaxx::misa_module_info misaxx_microbench::module_info() {
     info.set_version("1.0.0");
     info.set_name("misaxx-microbench");
     info.set_description("");
-    
+
     info.add_dependency(misaxx::module_info());
     info.add_dependency(misaxx::imaging::module_info());
     return info;
